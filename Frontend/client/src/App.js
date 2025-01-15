536e6a3d2b7229651969e4d26b562cb286070aff
quanghuy2090 <162445792+quanghuy2090@users.noreply.github.com>
2025-01-15 09:52:15 +0700

    feat: Create React Project

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
 
 const App = () => {
-  <div>
-    <p>rivera</p>
-  </div>
+  return (
+    <div>
+      <p>rivera</p>
+    </div>
+  )
 }
 
 export default App;
