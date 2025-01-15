4a0152b669cf4830891c91af49c7d8d8de8c9c00
quanghuy2090 <162445792+quanghuy2090@users.noreply.github.com>
2025-01-15 10:16:36 +0700

    feat: Chia Route

--- Frontend/client/src/App.js
+++ Frontend/client/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
+import {Route, Routes} from "react-router-dom"
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <p>rivera</p>
-    </div>
+    <Routes>
+      <Route path="/" Component={Home}/>
+      <Route path="/login" Component={Login}/>
+      <Route path="/register" Component={Register}/>
+    </Routes>
   )
 }
 
