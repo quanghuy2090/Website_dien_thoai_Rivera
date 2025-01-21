fde5e896b47949810e44d50fb4835551c780a3e2
quanghuy2090 <162445792+quanghuy2090@users.noreply.github.com>
2025-01-21 09:29:01 +0700

    change Frontend

---
+++ Frontend/client/src/App.tsx
@@ -0,0 +1,59 @@
+import { Link, useRoutes } from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+
+function App() {
+  const routes = useRoutes([
+   
+    { path: "register", element: <Register /> },
+    { path: "login", element: <Login /> },
+  ]);
+  return (
+    <>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+           
+          </Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul
+              className="navbar-nav me-auto mb-2 mb-lg-0"
+              style={{ gap: 3, fontSize: 20 }}
+            >
+              <li className="nav-item">
+                <Link className="nav-link active" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/register">
+                  Register
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">
+                  Login
+                </Link>
+              </li>
+              {/* <li className="nav-item">
+                <Link className="nav-link active" to="/product/add">
+                  Add Product
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link active" to="/product/list">
+                  List Product
+                </Link>
+              </li> */}
+            </ul>
+          </div>
+          <button className="btn btn-danger">Logout</button>
+        </div>
+      </nav>
+      <div className="container">{routes}</div>
+    </>
+  );
+}
+
+export default App;
