cb4a223c1e37dbb4f18be890a7d63a9d083e183a
khuongthanh29040 <khuongthanh290404@gmail.com>
2025-01-24 20:41:54 +0700

    upload file

--- Backend/src/routers/index.js
+++ Backend/src/routers/index.js
@@ -2,10 +2,11 @@ import express from "express";
 import routerAuth from "./auth.js";
 import routerCategory from "./category.js";
 import routerProduct from "./product.js";
+import routerFile from "./upload.js";
 const router = express.Router();
 
 router.use("/product", routerProduct);
 router.use("/category", routerCategory);
 router.use("/auth", routerAuth);
-
+router.use("/file", routerFile)
 export default router;
