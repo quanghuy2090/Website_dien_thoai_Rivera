53894d54ab846acc35efd1cb1c3d8db276ba6f21
khuongthanh29040 <khuongthanh290404@gmail.com>
2025-02-05 16:12:08 +0700

    validate form

--- Frontend/client/src/pages/Admin/Categories/AddCategories.tsx
+++ Frontend/client/src/pages/Admin/Categories/AddCategories.tsx
@@ -3,10 +3,17 @@ import { useForm } from 'react-hook-form'
 import { addCategories, Category } from '../../../services/category'
 import { useNavigate } from 'react-router-dom'
 import toast from 'react-hot-toast'
+import z from "zod"
+import { zodResolver } from '@hookform/resolvers/zod'
 
-
+const categorySchema = z.object({
+  name: z.string().min(3).max(225),
+  slug: z.string().min(3).max(225),
+})
 const AddCategories = () => {
-  const { register, handleSubmit } = useForm<Category>()
+  const { register, handleSubmit, formState: { errors } } = useForm<Category>({
+    resolver: zodResolver(categorySchema)
+  })
   const nav = useNavigate();
   const onSubmit = async (category: Category) => {
     try {
@@ -17,22 +24,24 @@ const AddCategories = () => {
     } catch (error) {
       console.log(error)
       toast.error("Error adding categories")
-    }  
+    }
   }
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)} >
         <div className='form-group'>
           <label htmlFor="name">name</label>
-          <input type="text" className='form-control' {...register("name")} />
+          <input type="text" className='form-control' {...register("name", { required: true })} />
+          {errors.name && <p className='text-danger'>{errors.name.message}</p>}
         </div>
-         <div className='form-group'>
+        <div className='form-group'>
           <label htmlFor="slug">slug</label>
-          <input type="text" className='form-control' {...register("slug")} />
+          <input type="text" className='form-control' {...register("slug", { required: true })} />
+          {errors.slug && <p className='text-danger'>{errors.slug.message}</p>}
         </div>
-         <div className='form-group'>
+        <div className='form-group'>
           <button className='btn btn-primary w-100'>submit</button>
-          
+
         </div>
       </form>
     </div>
--- Frontend/client/src/pages/Admin/Product/AddProduct.tsx
+++ Frontend/client/src/pages/Admin/Product/AddProduct.tsx
@@ -5,9 +5,19 @@ import { getCategories } from '../../../services/category';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
+import z from "zod"
+import { zodResolver } from '@hookform/resolvers/zod';
 const AddProduct = () => {
-  const { register, handleSubmit } = useForm<Product>();
+  const productSchema = z.object({
+    name: z.string().min(3).max(225),
+    price: z.number().min(0),
+    image: z.any().refine((file) => file !== null && file !== undefined),
+    description: z.string().nonempty(),
+    categoryId: z.string().nonempty(),
+  })
+  const { register, handleSubmit, formState: { errors } } = useForm<Product>({
+    resolver: zodResolver(productSchema)
+  });
   const [category, setCategory] = useState<Category[]>([]);
   const [imagePreview, setImagePreview] = useState<string | null>(null);
   const nav = useNavigate();
@@ -64,33 +74,39 @@ const AddProduct = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className='form-group'>
           <label htmlFor="name">Name</label>
-          <input type="text" className='form-control'{...register("name")} />
+          <input type="text" className='form-control'{...register("name", { required: true })} />
+          {errors.name && <p className='text-danger'>{errors.name.message}</p>}
         </div>
 
         <div className='form-group'>
           <label htmlFor="price">Price</label>
-          <input type="number" className='form-control'{...register("price")} />
+          <input type="number" className='form-control'{...register("price", { required: true, valueAsNumber: true })} />
+          {errors.price && <p className='text-danger'>{errors.price.message}</p>}
         </div>
 
         <div className='form-group'>
           <label htmlFor="description">Description</label>
-          <input type="text" className='form-control'{...register("description")} />
+          <input type="text" className='form-control'{...register("description", { required: true })} />
+          {errors.description && <p className='text-danger'>{errors.description.message}</p>}
         </div>
 
         <div>
           <label htmlFor="image">Image</label>
-          <input type="file" className='form-control'{...register("image")} onChange={handleImageChange} />
-          {imagePreview && <img src={imagePreview} alt="Image Preview" width="100" />}
+          <input type="file" className='form-control'{...register("image", { required: true })} onChange={handleImageChange} />
+          {errors.image && <p className='text-danger'>{errors.image.message}</p>}
+          {imagePreview && <img src={imagePreview} alt="Image Preview" width="150" />}
 
         </div>
         <div className='form-group'>
-          <select className='form-control'{...register("categoryId")}>
+          <label htmlFor="categories">Categories</label>
+          <select className='form-control'{...register("categoryId", { required: true })}>
             {category.map((item) => (
               <option key={item._id} value={item._id}>
                 {item.name}
               </option>
             ))}
           </select>
+          {errors.categoryId && <p className='text-danger'>{errors.categoryId.message}</p>}
         </div>
 
         <div className='form-group'>
