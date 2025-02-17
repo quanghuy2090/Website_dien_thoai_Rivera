b51ba048c4975260e50dc0a31ca4e513e50e863a
quanghuy2090 <162445792+quanghuy2090@users.noreply.github.com>
2025-02-17 14:31:57 +0700

    .

--- Frontend/client/src/pages/Homepage.tsx
+++ Frontend/client/src/pages/Homepage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAllProduct, Product } from "../services/product";
 import { ChevronLeft, ChevronRight } from "react-bootstrap-icons";
+import { Footer } from "../components/Footer";
 
 const HomePage = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -229,77 +230,7 @@ const HomePage = () => {
           </div>
         </div>{" "}
         {/* END OF IPAD PROMO */}
-        <footer>
-          <div id="footer-top">
-            <img
-              src="images/eLife.png"
-              alt="eLife Logo"
-              width="220px"
-              height="110px"
-            />
-            <div id="footer-socials-wrapper">
-              <div className="footer-social">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={40}
-                  height={40}
-                  fill="currentColor"
-                  className="bi bi-telephone-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
-                  />
-                </svg>
-                <span>012 345 6789</span>
-              </div>
-              <div className="footer-social">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={40}
-                  height={40}
-                  fill="currentColor"
-                  className="bi bi-telegram"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09" />
-                </svg>
-                <span>Telegram</span>
-              </div>
-              <div className="footer-social">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={40}
-                  height={40}
-                  fill="currentColor"
-                  className="bi bi-twitter"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15" />
-                </svg>
-                <span>Twitter</span>
-              </div>
-            </div>
-          </div>
-          <div id="footer-bottom">
-            <ul>
-              <li>
-                <a href="index.html">Home</a>
-              </li>
-              <li>
-                <a href="#">Products</a>
-              </li>
-              <li>
-                <a href="#">Services</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
-            </ul>
-            <p>Copyright © 2024 eLife. All rights reserved.</p>
-          </div>
-        </footer>
+        <Footer/>
       </div>
     </div>
   );
