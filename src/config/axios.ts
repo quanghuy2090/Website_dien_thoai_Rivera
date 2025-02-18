ab1a4cdb2beedb0510867805b1e185ac7e77a2f8
hanhkt.ndm <khuonghanh88.ndm.@gmail.com>
2025-02-18 09:02:45 +0700

    tesst

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -5,6 +5,7 @@ const token = localStorage.getItem("token");
 export const http = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
+    
     Authorization: token ? `Bearer ${token}` : null,
   },
 });
