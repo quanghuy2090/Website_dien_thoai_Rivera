f3ab23bbf05919d275a3bd27a5d4d48743604893
quanghuy2090 <162445792+quanghuy2090@users.noreply.github.com>
2025-02-26 14:27:14 +0700

    .

--- Frontend/client/src/pages/ProductDetail.tsx
+++ Frontend/client/src/pages/ProductDetail.tsx
@@ -255,6 +255,206 @@ const ProductDetail = () => {
                 <i className="fa fa-shopping-cart mr-1" /> Thêm giỏ hàng
               </button>
             </div>
+            <div className="d-flex align-items-center mb-4 pt-2">
+              <div className="input-group quantity mr-3" style={{ width: 130 }}>
+                <div className="input-group-btn">
+                  <button className="btn btn-primary btn-minus">
+                    <i className="fa fa-minus" />
+                  </button>
+                </div>
+                <input
+                  type="text"
+                  className="form-control bg-secondary text-center"
+                  defaultValue={1}
+                />
+                <div className="input-group-btn">
+                  <button className="btn btn-primary btn-plus">
+                    <i className="fa fa-plus" />
+                  </button>
+                </div>
+              </div>
+              <button className="btn btn-primary px-3">
+                <i className="fa fa-shopping-cart mr-1" /> Thêm giỏ hàng
+              </button>
+            </div>
+            {/* <div className="d-flex pt-2">
+        <p className="text-dark font-weight-medium mb-0 mr-2">Share on:</p>
+        <div className="d-inline-flex">
+          <a className="text-dark px-2" href="">
+            <i className="fab fa-facebook-f" />
+          </a>
+          <a className="text-dark px-2" href="">
+            <i className="fab fa-twitter" />
+          </a>
+          <a className="text-dark px-2" href="">
+            <i className="fab fa-linkedin-in" />
+          </a>
+          <a className="text-dark px-2" href="">
+            <i className="fab fa-pinterest" />
+          </a>
+        </div>
+      </div> */}
+          </div>
+        </div>
+        <div className="row px-xl-5">
+          <div className="col">
+            <div className="nav nav-tabs justify-content-center border-secondary mb-4">
+              <a
+                className="nav-item nav-link active"
+                data-toggle="tab"
+                href="#tab-pane-1"
+              >
+                Giới thiệu
+              </a>
+              <a
+                className="nav-item nav-link"
+                data-toggle="tab"
+                href="#tab-pane-2"
+              >
+                Thông tin sản phẩm
+              </a>
+              <a
+                className="nav-item nav-link"
+                data-toggle="tab"
+                href="#tab-pane-3"
+              >
+                Đánh giá (0)
+              </a>
+            </div>
+            <div className="tab-content">
+              <div className="tab-pane fade show active" id="tab-pane-1">
+                <p>{product?.description}</p>
+              </div>
+              <div className="tab-pane fade" id="tab-pane-2">
+                <h4 className="mb-3">Additional Information</h4>
+                <div className="row">
+                  <div className="col-md-6">
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item px-0">
+                        Sit erat duo lorem duo ea consetetur, et eirmod
+                        takimata.
+                      </li>
+                      <li className="list-group-item px-0">
+                        Amet kasd gubergren sit sanctus et lorem eos sadipscing
+                        at.
+                      </li>
+                      <li className="list-group-item px-0">
+                        Duo amet accusam eirmod nonumy stet et et stet eirmod.
+                      </li>
+                      <li className="list-group-item px-0">
+                        Takimata ea clita labore amet ipsum erat justo voluptua.
+                        Nonumy.
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="col-md-6">
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item px-0">
+                        Sit erat duo lorem duo ea consetetur, et eirmod
+                        takimata.
+                      </li>
+                      <li className="list-group-item px-0">
+                        Amet kasd gubergren sit sanctus et lorem eos sadipscing
+                        at.
+                      </li>
+                      <li className="list-group-item px-0">
+                        Duo amet accusam eirmod nonumy stet et et stet eirmod.
+                      </li>
+                      <li className="list-group-item px-0">
+                        Takimata ea clita labore amet ipsum erat justo voluptua.
+                        Nonumy.
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              {/* <div className="tab-pane fade" id="tab-pane-3">
+                <div className="row">
+                  <div className="col-md-6">
+                    <h4 className="mb-4">
+                      1 review for "Colorful Stylish Shirt"
+                    </h4>
+                    <div className="media mb-4">
+                      <img
+                        src="img/user.jpg"
+                        alt="Image"
+                        className="img-fluid mr-3 mt-1"
+                        style={{ width: 45 }}
+                      />
+                      <div className="media-body">
+                        <h6>
+                          John Doe
+                          <small>
+                            {" "}
+                            - <i>01 Jan 2045</i>
+                          </small>
+                        </h6>
+                        <div className="text-primary mb-2">
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star" />
+                          <i className="fas fa-star-half-alt" />
+                          <i className="far fa-star" />
+                        </div>
+                        <p>
+                          Diam amet duo labore stet elitr ea clita ipsum, tempor
+                          labore accusam ipsum et no at. Kasd diam tempor rebum
+                          magna dolores sed sed eirmod ipsum.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <h4 className="mb-4">Leave a review</h4>
+                    <small>
+                      Your email address will not be published. Required fields
+                      are marked *
+                    </small>
+                    <div className="d-flex my-3">
+                      <p className="mb-0 mr-2">Your Rating * :</p>
+                      <div className="text-primary">
+                        <i className="far fa-star" />
+                        <i className="far fa-star" />
+                        <i className="far fa-star" />
+                        <i className="far fa-star" />
+                        <i className="far fa-star" />
+                      </div>
+                    </div>
+                    <form>
+                      <div className="form-group">
+                        <label htmlFor="message">Your Review *</label>
+                        <textarea
+                          id="message"
+                          cols={30}
+                          rows={5}
+                          className="form-control"
+                          defaultValue={""}
+                        />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="name">Your Name *</label>
+                        <input type="text" className="form-control" id="name" />
+                      </div>
+                      <div className="form-group">
+                        <label htmlFor="email">Your Email *</label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="email"
+                        />
+                      </div>
+                      <div className="form-group mb-0">
+                        <input
+                          type="submit"
+                          defaultValue="Leave Your Review"
+                          className="btn btn-primary px-3"
+                        />
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div> */}
+            </div>
           </div>
         </div>
 
