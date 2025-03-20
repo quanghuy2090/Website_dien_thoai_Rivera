181bd7b0d49e73014a68702f152cdf19adc557d4
hanhkt.ndm <khuonghanh88.ndm.@gmail.com>
2025-03-20 09:34:07 +0700

    update code

--- Frontend/client/src/pages/Client/Cart.tsx
+++ Frontend/client/src/pages/Client/Cart.tsx
@@ -88,73 +88,55 @@ const Cart = () => {
         </div>
         {/* /container */}
       </div>
-      {/* Page Header End */}
-      {/* Cart Start */}
-      <div className="container-fluid pt-5">
-        <div className="row px-xl-5">
-          <div className="col-lg-8 table-responsive mb-5">
-            <table className="table table-bordered text-center mb-0">
-              <thead className="bg-secondary text-dark">
+      {/* /BREADCRUMB */}
+      <div className="cart-container">
+        {/* 🛒 Cart Header */}
+        {/* <div className="cart-header">
+        <h2>🛒 Giỏ hàng của bạn</h2>
+      </div> */}
+
+        {/* 📋 Cart Table */}
+        <div className="cart-table-container">
+          {carts.length > 0 ? (
+            <table className="cart-table">
+              <thead>
                 <tr>
                   <th>Sản phẩm</th>
                   <th>Giá gốc</th>
                   <th>Số lượng</th>
-                  <th>Thành giá</th>
+                  <th>Thành tiền</th>
                   <th>Hủy</th>
                 </tr>
               </thead>
-              <tbody className="align-middle">
-                {carts.map((item) => (
-                  <tr>
-                    <td className="align-middle">
-                      {item.productId?.images && (
-                        <img src={item.productId.images[0]} alt="" width={50} />
-                      )}
-                      <Link className="table align-middle" to={`/product/${item.productId._id}`}>{item.productId?.name}</Link>
-                    </td>
-                    <td className="align-middle">
-                      {formatPrice(item.productId?.price)}
-                    </td>
-                    <td className="align-middle">
-                      <div
-                        className="input-group quantity mx-auto"
-                        style={{ width: 100 }}
-                      >
-                        <div className="input-group-btn">
-                          <button
-                            className="btn btn-sm btn-primary btn-minus"
-                            onClick={() =>
-                              handleUpdateCart(item.productId._id, -1)
-                            }
-                            disabled={item.quantity <= 1}
-                          >
-                            <i className="fa fa-minus" />
-                          </button>
-                        </div>
-                        <input
-                          type="text"
-                          className="form-control form-control-sm bg-secondary text-center"
-                          value={item.quantity}
-                        />
-                        <div className="input-group-btn">
-                          <button
-                            className="btn btn-sm btn-primary btn-plus"
-                            onClick={() =>
-                              handleUpdateCart(item.productId._id, 1)
-                            }
-                          >
-                            <i className="fa fa-plus" />
-                          </button>
-                        </div>
+              <tbody>
+                {carts.map((item, index) => (
+                  <tr key={index}>
+                    <td className="product-info">
+                      <img
+                        src={item.image}
+                        alt={item.name}
+                        className="cart-product-image"
+                      />
+                      <div>
+                        <strong>{item.name}</strong>
+                        <p>
+                          {typeof item.variants?.color === "string"
+                            ? item.variants.color
+                            : item.variants.color.name}{" "}
+                          /{" "}
+                          {typeof item.variants?.capacity === "string"
+                            ? item.variants.capacity
+                            : item.variants.capacity.value}
+                        </p>
                       </div>
                     </td>
-                    <td className="align-middle">
-                      {formatPrice(item.productId.price * item.quantity)}
-                    </td>
-                    <td className="align-middle">
+                    <td>{formatPrice(item.variants.price)}</td>
+                    <td>{item.quantity}</td>
+                    <td>{formatPrice(item.subtotal)}</td>
+                    <td>
                       <button
-                        className="btn btn-sm btn-primary"
-                        onClick={() => removeCart(item.productId._id)}
+                        className="remove-btn"
+                        onClick={() => handleRemoveFromCart(item.productId)}
                       >
                         ❌
                       </button>
@@ -163,48 +145,12 @@ const Cart = () => {
                 ))}
               </tbody>
             </table>
-          </div>
-          <div className="col-lg-4">
-            {/* <form className="mb-5" action="">
-              <div className="input-group">
-                <input
-                  type="text"
-                  className="form-control p-4"
-                  placeholder="Coupon Code"
-                />
-                <div className="input-group-append">
-                  <button className="btn btn-primary">Apply Coupon</button>
-                </div>
-              </div>
-            </form> */}
-            <div className="card border-secondary mb-5">
-              <div className="card-header bg-secondary border-0">
-                <h4 className="font-weight-semi-bold m-0">Tổng quan</h4>
-              </div>
-              {/* <div className="card-body">
-                <div className="d-flex justify-content-between mb-3 pt-1">
-                  <h6 className="font-weight-medium">Subtotal</h6>
-                  <h6 className="font-weight-medium">$150</h6>
-                </div>
-                <div className="d-flex justify-content-between">
-                  <h6 className="font-weight-medium">Shipping</h6>
-                  <h6 className="font-weight-medium">$10</h6>
-                </div>
-              </div> */}
-              <div className="card-footer border-secondary bg-transparent">
-                <div className="d-flex justify-content-between mt-2">
-                  <h5 className="font-weight-bold">Tổng</h5>
-                  <h5 className="font-weight-bold">
-                    {formatPrice(totalAmount)}
-                  </h5>
-                </div>
-                <Link
-                  to={`/checkout`}
-                  className="btn btn-block btn-primary my-3 py-3"
-                >
-                  Thanh Toán
-                </Link>
-              </div>
+          ) : (
+            <div className="empty-cart">
+              <p>Giỏ hàng của bạn đang trống! 🛒</p>
+              <Link to="/" className="continue-shopping">
+                Tiếp tục mua sắm
+              </Link>
             </div>
           )}
         </div>
@@ -224,8 +170,7 @@ const Cart = () => {
           </Link>
         </div>
       </div>
-      {/* Cart End */}
-    </div>
+    </>
   );
 };
 
--- Frontend/client/src/pages/Client/ProductDetail.tsx
+++ Frontend/client/src/pages/Client/ProductDetail.tsx
@@ -188,201 +188,110 @@ const ProductDetail = () => {
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-lg-7 pb-5">
-            <h3 className="h4 font-weight-bold">{product?.name}</h3>
-            <div className="d-flex align-items-center mb-2">
-              <div className="text-danger">
-                <i className="fas fa-star"> </i>
-                <i className="fas fa-star"> </i>
-                <i className="fas fa-star"> </i>
-                <i className="fas fa-star"> </i>
-                <i className="fas fa-star-half-alt"> </i>
-              </div>
-              <span className="ml-2 text-muted">
-                (10 Review(s)) |
-                <a className="text-primary" href="#">
-                  {" "}
-                  Add your review{" "}
-                </a>
-              </span>
-            </div>
-            <h2 className="h3 text-danger font-weight-bold mb-2">
-              {formatPrice(product?.price ?? 0)}
-            </h2>
-            <div className="text-success font-weight-bold mb-4">IN STOCK</div>
-            <p className="mb-4">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <div className="product-options">
-              <div className="row align-items-center mb-3">
-                <div className="col-auto">
-                  <label className="form-label fw-bold">Dung Lượng</label>
+            {/* /Product thumb imgs */}
+            <div className="col-md-5">
+              <div className=" product-details">
+                <div className="">
+                  <h3 className="h3 font-weight-bold">{product?.name}</h3>
+                  <h3 className="h5 mt-2">SKU: {selectedVariant?.sku}</h3>
                 </div>
-                <div className="col-auto">
-                  <select className="form-select form-select-sm w-auto border-dark">
-                    <option value="0">64GB</option>
-                    <option value="1">128GB</option>
-                    <option value="2">256GB</option>
-                  </select>
+                <div className="d-flex align-items-center mb-2">
+                  <div style={{ color: "yellow" }}>
+                    <i className="fas fa-star"> </i>
+                    <i className="fas fa-star"> </i>
+                    <i className="fas fa-star"> </i>
+                    <i className="fas fa-star"> </i>
+                    <i className="fas fa-star-half-alt"> </i>
+                  </div>
+                  <span className="ml-2 text-muted">
+                    (10 Review(s)) |
+                    <a className="text-primary" href="#">
+                      {" "}
+                      Add your review{" "}
+                    </a>
+                  </span>
                 </div>
-                <div className="col-auto">
-                  <label className="form-label fw-bold">Màu Sắc</label>
+                <div className="variant-selector mt-3">
+                  {product?.variants.map((variant, index) => (
+                    <button
+                      key={index}
+                      className={`variant-btn ${variant === selectedVariant ? "active" : ""
+                        }`}
+                      onClick={() => handleVariantChange(variant)}
+                    >
+                      {typeof variant.color === 'string' ? variant.color : variant.color.name} - {typeof variant.capacity === 'string' ? variant.capacity : variant.capacity.value}
+                    </button>
+                  ))}
                 </div>
-                <div className="col-auto">
-                  <select className="form-select form-select-sm w-auto border-dark">
-                    <option value="0">Đỏ</option>
-                    <option value="1">Đen</option>
-                    <option value="2">Trắng</option>
-                  </select>
+                <h2 className="h3 font-weight-bold mb-2">
+                  {formatPrice(selectedVariant?.price ?? 0)}
+                </h2>
+                <div
+                  className={`font-weight-bold mb-4 ${selectedVariant?.stock > 0 ? "text-success" : "text-danger"
+                    }`}
+                >
+                  {selectedVariant?.stock > 0 ? "IN STOCK" : "OUT OF STOCK"}: {selectedVariant?.stock}
                 </div>
-              </div>
-            </div>
-
-            <div className="d-flex align-items-center mb-4 pt-2">
-              {/* Nút Giảm */}
-              <button
-                className="btn btn-primary me-3"
-                onClick={decreaseQuantity}
-              >
-                <i className="fa fa-minus"></i>
-              </button>
-
-              {/* Ô Input */}
-              <div
-                className="d-flex align-items-center border border-dark rounded px-3 bg-light"
-                style={{ width: 80, height: 38 }}
-              >
-                <input
-                  type="text"
-                  className="form-control text-center border-0 bg-light"
-                  style={{ width: "100%" }}
-                  value={quantity}
-                  readOnly
-                />
-              </div>
-
-              {/* Nút Tăng */}
-              <button
-                className="btn btn-primary ms-3"
-                onClick={increaseQuantity}
-              >
-                <i className="fa fa-plus"></i>
-              </button>
-            </div>
-
-            <br />
-            <div>
-              <button
-                className="btn btn-danger px-3 rounded-pill"
-                onClick={() => addToCart(product?._id)}
-              >
-                <i className="fa fa-shopping-cart me-1" /> Thêm giỏ hàng
-              </button>
-            </div>
-
-            {/* <div className="d-flex align-items-center mb-4 pt-2">
-              <div className="input-group quantity mr-3" style={{ width: 130 }}>
-                <div className="input-group-btn">
-                  <button className="btn btn-primary btn-minus">
-                    <i className="fa fa-minus" />
-                  </button>
+                <div className="product-options">
+                  <label>
+                    Dung lượng:{" "}
+                    <span className="ms-2">{typeof selectedVariant?.capacity === 'string' ? selectedVariant.capacity : selectedVariant?.capacity.value}</span>
+                  </label>
+                  <label>
+                    Màu sắc:{" "}
+                    <span className="ms-2">{typeof selectedVariant?.color === 'string' ? selectedVariant.color : selectedVariant?.color.name}</span>
+                  </label>
                 </div>
-                <input
-                  type="text"
-                  className="form-control bg-secondary text-center"
-                  defaultValue={1}
-                />
-                <div className="input-group-btn">
-                  <button className="btn btn-primary btn-plus">
-                    <i className="fa fa-plus" />
+
+                <div className="add-to-cart">
+                  <div className="qty-label">
+                    <span className="me-2">Số lượng: </span>
+                    <div className="input-number">
+                      <input type="number" value={quantity} readOnly />
+                      <span className="qty-up" onClick={increaseQuantity}>
+                        +
+                      </span>
+                      <span className="qty-down" onClick={decreaseQuantity}>
+                        -
+                      </span>
+                    </div>
+                  </div>
+                  <button
+                    className="add-to-cart-btn"
+                    onClick={() => addToCart(product?._id)}
+                  >
+                    <i className="fa fa-shopping-cart" /> add to cart
                   </button>
                 </div>
               </div>
             </div>
-            <div className="tab-content">
-              <div className="tab-pane fade show active" id="tab-pane-1">
-                <h4 className="mb-3">Additional Information</h4>
-                <div className="row">
-                  <div className="col-md-6">
-                    <ul className="list-group list-group-flush">
-                      <li className="list-group-item px-0">
-                        Sit erat duo lorem duo ea consetetur, et eirmod
-                        takimata.
-                      </li>
-                      <li className="list-group-item px-0">
-                        Amet kasd gubergren sit sanctus et lorem eos sadipscing
-                        at.
-                      </li>
-                      <li className="list-group-item px-0">
-                        Duo amet accusam eirmod nonumy stet et et stet eirmod.
-                      </li>
-                      <li className="list-group-item px-0">
-                        Takimata ea clita labore amet ipsum erat justo voluptua.
-                        Nonumy.
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="col-md-6">
-                    <ul className="list-group list-group-flush">
-                      <li className="list-group-item px-0">
-                        Sit erat duo lorem duo ea consetetur, et eirmod
-                        takimata.
-                      </li>
-                      <li className="list-group-item px-0">
-                        Amet kasd gubergren sit sanctus et lorem eos sadipscing
-                        at.
-                      </li>
-                      <li className="list-group-item px-0">
-                        Duo amet accusam eirmod nonumy stet et et stet eirmod.
-                      </li>
-                      <li className="list-group-item px-0">
-                        Takimata ea clita labore amet ipsum erat justo voluptua.
-                        Nonumy.
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="tab-pane fade" id="tab-pane-2">
-                <p>{product?.description}</p>
-              </div>
-              {/* <div className="tab-pane fade" id="tab-pane-3">
-                <div className="row">
-                  <div className="col-md-6">
-                    <h4 className="mb-4">
-                      1 review for "Colorful Stylish Shirt"
-                    </h4>
-                    <div className="media mb-4">
-                      <img
-                        src="img/user.jpg"
-                        alt="Image"
-                        className="img-fluid mr-3 mt-1"
-                        style={{ width: 45 }}
-                      />
-                      <div className="media-body">
-                        <h6>
-                          John Doe
-                          <small>
-                            {" "}
-                            - <i>01 Jan 2045</i>
-                          </small>
-                        </h6>
-                        <div className="text-primary mb-2">
-                          <i className="fas fa-star" />
-                          <i className="fas fa-star" />
-                          <i className="fas fa-star" />
-                          <i className="fas fa-star-half-alt" />
-                          <i className="far fa-star" />
-                        </div>
-                        <p>
-                          Diam amet duo labore stet elitr ea clita ipsum, tempor
-                          labore accusam ipsum et no at. Kasd diam tempor rebum
-                          magna dolores sed sed eirmod ipsum.
-                        </p>
+            {/* Product tab */}
+            <div className="col-md-12">
+              <div id="product-tab">
+                {/* product tab nav */}
+                <ul className="tab-nav">
+                  <li className={activeTab === "tab01" ? "active" : ""}>
+                    <a onClick={() => handleTabClick("tab01")}>Description</a>
+                  </li>
+                  <li className={activeTab === "tab02" ? "active" : ""}>
+                    <a onClick={() => handleTabClick("tab02")}>Details</a>
+                  </li>
+                  <li className={activeTab === "tab03" ? "active" : ""}>
+                    <a onClick={() => handleTabClick("tab03")}>Reviews (3)</a>
+                  </li>
+                </ul>
+                {/* /product tab nav */}
+                {/* product tab content */}
+                <div className="tab-content">
+                  {/* Tab 1: Description */}
+                  <div
+                    id="tab01"
+                    className={`tab-pane ${activeTab === "tab01" ? "active" : "fade"
+                      }`}
+                  >
+                    <div className="row">
+                      <div className="col-md-12">
+                        <p>{product?.short_description}</p>
                       </div>
                     </div>
                   </div>
