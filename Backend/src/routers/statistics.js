85ced16bae26e4ee44a71473636b32fd1b1b7854
TCSon03 <truongcongsson@gmail.com>
2025-04-07 19:52:28 +0700

    feat: statistics

---
+++ Backend/src/routers/statistics.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  getTotalRevenue,
+  getTopUsers,
+  getTopProducts,
+  getLeastSoldProducts,
+} from "../controllers/statistics.js";
+import { checkAdminPermission } from "../middlewares/checkAdminPermission.js";
+
+const routerStatistics = express.Router();
+
+// Tất cả các route thống kê đều yêu cầu quyền admin
+routerStatistics.get("/revenue", checkAdminPermission, getTotalRevenue);
+routerStatistics.get("/top-users", checkAdminPermission, getTopUsers);
+routerStatistics.get("/top-products", checkAdminPermission, getTopProducts);
+routerStatistics.get(
+  "/least-sold-products",
+  checkAdminPermission,
+  getLeastSoldProducts
+);
+
+export default routerStatistics;
